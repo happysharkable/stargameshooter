18739d084f23394ec87845cc368bd6bef5e20bbb
Alexey Kutepov <alkutepov@gmail.com>
2020-03-23 19:50:25 +0300

    Lesson 2 part 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -17,6 +18,42 @@ public class StarGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		region = new TextureRegion(img, 10, 10, 150, 150);
+
+		Vector2 v1 = new Vector2(1f, 3f);
+		Vector2 v2 = new Vector2(2f, 4f);
+		v1.add(v2);
+		System.out.println("v1 add v2 = " + v1.x + " " + v1.y);
+
+		v1.set(5f, 7f);
+		v2.set(3f, 3f);
+		v1.sub(v2);
+		System.out.println("v1 sub v2 = " + v1.x + " " + v1.y);
+
+		v1.len();
+		System.out.println("v1 len = " + v1.len());
+
+		v1.nor();
+		System.out.println("v1 len = " + v1.len());
+
+		v1.set(5f, 7f);
+		v2.set(3f, 3f);
+		Vector2 v3 = v1.cpy().sub(v2);
+		System.out.println("v1 = " + v1.x + " " + v1.y);
+		System.out.println("v3 = " + v3.x + " " + v3.y);
+
+		System.out.println("v1 len = " + v1.len());
+		v1.scl(0.2f);
+		System.out.println("v1 = " + v1.x + " " + v1.y);
+		System.out.println("v1 len = " + v1.len());
+
+
+		System.out.println("v1.dot(v2) = " + v1.dot(v2));
+
+		v1.set(1, 1);
+		v2.set(2, 2);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
 	}
 
 	@Override
