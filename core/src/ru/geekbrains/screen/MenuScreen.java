176a479e3d8acf4bd65078d6fab7adbf9b55f91c
Alexey Kutepov <alkutepov@gmail.com>
2020-03-26 19:15:28 +0300

    Lesson 3 home work

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,25 +3,28 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 0.5f;
+
     private Texture img;
+    private Vector2 touch;
     private Vector2 pos;
     private Vector2 v;
-//    private float rotate;
+    private Vector2 tmp;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        touch = new Vector2();
         pos = new Vector2();
-        v = new Vector2(0.3f, 0.3f);
-//        rotate = 0;
+        v = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
@@ -39,13 +42,20 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return false;
     }
 
     private void update(float delta) {
-        pos.add(v);
-//        rotate += 1;
+        tmp.set(touch);
+        float remainingDistance = (tmp.sub(pos)).len();
+        if (remainingDistance > V_LEN) {
+            pos.add(v);
+        } else {
+            v.setZero();
+            pos.set(touch);
+        }
     }
 
     private void draw() {
@@ -53,7 +63,6 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, pos.x, pos.y);
-//        batch.draw(new TextureRegion(img), pos.x, pos.y, pos.x, pos.y, 250, 250, 1, 1, rotate);
         batch.end();
     }
 
